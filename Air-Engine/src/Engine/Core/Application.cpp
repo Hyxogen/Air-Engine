4b92dc373702cb2acd2943fb3712fdb5586892ac
Daan Meijer <daansander02@outlook.com>
2021-03-23 18:03:22 +0100

    Clean up Application header

--- Air-Engine/src/Engine/Core/Application.cpp
+++ Air-Engine/src/Engine/Core/Application.cpp
@@ -1,25 +1,40 @@
 #include "Application.hpp"
 #include "../../Platform/Windows/WindowsWindow.hpp"
+#include "../../Platform/Windows/Console.hpp"
 
 namespace engine {
 	namespace core {
 
 		Application::Application() {
-			mWindow = new platform::windows::WindowsWindow(500, 600, L"Application");
-			mWindow->Initialize();
-
-			mWindow->SetVisibility(AIR_W_SHOW);
+			if (!Initialize())
+				Run();
 		}
 
 		Application::~Application() {
 			delete mWindow;
 		}
 
+		bool Application::Initialize() {
+			mConsole = new platform::windows::Console();
+			if (mConsole->Initialize())
+				return true;
+
+			mWindow = new platform::windows::WindowsWindow(500, 600, L"Application");
+			mWindow->Initialize();
+
+			mWindow->SetVisibility(AIR_W_SHOW);
+			return false;
+		}
+
 		bool Application::Run() {
 			while (!mWindow->ShouldClose()) {
-				mWindow->Update();
+				Update();
 			}
 			return true;
+		}
+
+		void Application::Update() {
+			mWindow->Update();
 		}
 
 	}
--- Air-Engine/src/Engine/Core/Application.hpp
+++ Air-Engine/src/Engine/Core/Application.hpp
@@ -1,20 +1,34 @@
 #pragma once
 
-#include "../IO/Window.hpp"
+namespace platform {
+	namespace windows {
+		class Console;
+	}
+}
 
 namespace engine {
 	namespace core {
 
+		class Window;
+
 		class Application {
 		protected:
 			Window* mWindow;
+			platform::windows::Console* mConsole;
 
 		public:
 			Application();
 
 			virtual ~Application();
 
+			//Returns false if no errors 
+			bool Initialize();
+
 			bool Run();
+
+			void Update();
+
+			inline Window* GetWindow() const { return mWindow; }
 		};
 
 		Application* CreateApplication();
