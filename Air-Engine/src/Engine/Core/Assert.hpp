7503505b5e60832ee6ae40895ddc721da21738aa
Daan Meijer <daansander02@outlook.com>
2021-04-07 18:21:36 +0200

    Add function to check if a type is of a type

--- Air-Engine/src/Engine/Core/Assert.hpp
+++ Air-Engine/src/Engine/Core/Assert.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#ifdef AIR_PLATFORM_WINDOWS
 #ifdef ASSERTIONS_ENABLED
+#ifdef AIR_PLATFORM_WINDOWS
 #define ASSERT(expr) \
 	if (expr) {} \
 	else {\
@@ -10,4 +10,13 @@
 #else
 #define ASSERT(expr)
 #endif
+template<class T1, class T2> struct Can_copy {
+	static void Constraints(T1 a, T2 b) {
+		b = a;
+	}
+
+	Can_copy() {
+		void(*p)(T1, T2) = Constraints;
+	}
+};
 #endif
