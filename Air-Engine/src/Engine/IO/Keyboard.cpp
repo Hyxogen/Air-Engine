4e30e4cdb1dde120f46482dc51d4a392b090632e
Daan Meijer <daansander02@outlook.com>
2021-04-08 18:43:12 +0200

    Add base keyboard class

---
+++ Air-Engine/src/Engine/IO/Keyboard.cpp
@@ -0,0 +1,8 @@
+#include "Keyboard.hpp"
+
+namespace engine {
+	namespace io {
+		Keyboard::Keyboard(Window* window) : mWindow(window) {
+		}
+	}
+}
---
+++ Air-Engine/src/Engine/IO/Keyboard.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace engine {
+	namespace io {
+
+		class Window;
+
+		class Keyboard {
+			Window* mWindow;
+		public:
+			Keyboard(Window* window);
+
+			virtual bool GetKeyDown(int keyCode) const = 0;
+
+			virtual bool GetKeyToggled(int keyCode) const = 0;
+
+			inline Window* GetWindow() const { return mWindow; }
+		}
+} }
