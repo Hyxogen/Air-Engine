1fdaf46718e11509c566a3159b057068cc5149ac
Daan Meijer <daansander02@outlook.com>
2021-04-20 21:12:42 +0200

    Remove comments

--- Air-Engine/src/Platform/Windows/WindowsWindow.cpp
+++ Air-Engine/src/Platform/Windows/WindowsWindow.cpp
@@ -85,15 +85,12 @@ namespace platform {
 				WindowsWindow* window = reinterpret_cast<WindowsWindow*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
 				WindowsKeyDownEvent keyEvent(window, uMsg, wParam, lParam);
 				engine::core::Application::GetApplication()->GetDispatcher()->Dispatch(keyEvent);
-				//delete keyEvent;
 				return 0;
 			}
 			else if (uMsg == WM_KEYUP) {
 				WindowsWindow* window = reinterpret_cast<WindowsWindow*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
-				//Since the event handler is not buffer, does this even need to be on the heap?
 				WindowsKeyReleaseEvent keyEvent(window, uMsg, wParam, lParam);
 				engine::core::Application::GetApplication()->GetDispatcher()->Dispatch(keyEvent);
-				//delete keyEvent;
 				return 0;
 			}
 			else if (uMsg == WM_LBUTTONDOWN || uMsg == WM_LBUTTONUP || uMsg == WM_MBUTTONDOWN || uMsg == WM_MBUTTONUP || uMsg == WM_RBUTTONDOWN || uMsg == WM_RBUTTONUP || uMsg == WM_XBUTTONDOWN ||
