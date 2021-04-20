e6783e9ec3a1a56b3b15df3718d1e212db777d41
Daan Meijer <daansander02@outlook.com>
2021-04-20 16:10:59 +0200

    Add basic key events

--- Air-Engine/src/Engine/Events/KeyEvent.hpp
+++ Air-Engine/src/Engine/Events/KeyEvent.hpp
@@ -2,6 +2,7 @@
 
 #include "Event.hpp"
 #include "../IO/KeyCodes.hpp"
+#include "../Util/HashUtils.hpp"
 
 namespace engine {
 	namespace events {
@@ -10,12 +11,31 @@ namespace engine {
 		protected:
 			io::KeyCode m_KeyCode;
 
-			KeyEvent(io::KeyCode keyCode);
+			KeyEvent(io::KeyCode keyCode) : m_KeyCode(keyCode) {}
 
 		public:
 			inline io::KeyCode GetKeyCode() const { return m_KeyCode; }
 
 			virtual unsigned int GetID() const = 0;
+		};
+
+		class KeyDownEvent : public KeyEvent {
+		protected:
+			unsigned short m_RepeatCount;
+
+		public:
+			KeyDownEvent(io::KeyCode keyCode, unsigned short repeatCount) : KeyEvent(keyCode), m_RepeatCount(repeatCount) {}
+
+			unsigned int GetID() const override { return Hash("EVENT_KEY_DOWN", 15) }
+
+			inline unsigned short GetRepeatCount() const { return m_RepeatCount; }
+		};
+
+		class KeyReleaseEvent : public KeyEvent {
+		public:
+			KeyReleaseEvent(io::KeyCode keyCode) : KeyEvent(keyCode) {}
+
+			unsigned int GetID() const override { return Hash("EVENT_KEY_RELEASE", 18) }
 		};
 	}
 }
