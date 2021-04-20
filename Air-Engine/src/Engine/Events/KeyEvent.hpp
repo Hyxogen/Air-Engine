ddb58e0536e00519a23315978ede731618f9b463
Daan Meijer <daansander02@outlook.com>
2021-04-20 16:01:42 +0200

    Add base class for key events

--- Air-Engine/src/Engine/Events/KeyEvent.hpp
+++ Air-Engine/src/Engine/Events/KeyEvent.hpp
@@ -1,12 +1,21 @@
 #pragma once
 
 #include "Event.hpp"
+#include "../IO/KeyCodes.hpp"
 
 namespace engine {
 	namespace events {
 
-		class KeyEvents : public Event {
+		class KeyEvent : public Event {
 		protected:
+			io::KeyCode m_KeyCode;
+
+			KeyEvent(io::KeyCode keyCode);
+
+		public:
+			inline io::KeyCode GetKeyCode() const { return m_KeyCode; }
+
+			virtual unsigned int GetID() const = 0;
 		};
 	}
 }
