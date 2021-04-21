cfcc35078c3cddccc2170b8c5f2a314b44da17a9
Daan Meijer <daansander02@outlook.com>
2021-04-21 16:06:34 +0200

    Add windows mousecodes

--- Air-Engine/src/Engine/IO/MouseCodes.hpp
+++ Air-Engine/src/Engine/IO/MouseCodes.hpp
@@ -1,10 +1,23 @@
 #pragma once
 
+#ifdef AIR_PLATFORM_WINDOWS
+#include "../../Platform/Windows/WindowsMouseCodes.hpp"
+#else
+#error Air Engine only supports windows at the moment
+#endif
+
 namespace engine {
 	namespace io {
 
 		enum class MouseCode : unsigned int {
-			
+			#ifdef AIR_PLATFORM_WINDOWS
+			LEFT			= platform::windows::MouseCode::LEFT,
+			MIDDLE			= platform::windows::MouseCode::MIDDLE,
+			RIGHT			= platform::windows::MouseCode::RIGHT,
+
+			XONE			= platform::windows::MouseCode::XONE,
+			XTWO			= platform::windows::MouseCode::XTWO
+			#endif
 		};
 
 	}
