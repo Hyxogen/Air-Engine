b80c4d18c577207a2dfabaa6bf4f4c59cfede583
Daan Meijer <daansander02@outlook.com>
2021-04-21 16:36:51 +0200

    Add documentation

--- Air-Engine/src/Engine/Util/HashUtils.hpp
+++ Air-Engine/src/Engine/Util/HashUtils.hpp
@@ -8,6 +8,13 @@
 
 #ifdef AIR_HASH_FNV1A
 #ifdef AIR_HASH_32BIT
+/// <summary>
+/// Non-cryptografic hash function that can be resolved at compile time
+/// </summary>
+/// <param name="str">String to hash</param>
+/// <param name="n">Length of string in characters</param>
+/// <param name="basis">Number for hashing see FNV1A algorithm for more</param>
+/// <returns>Unique unsigned int for str</returns>
 constexpr unsigned int Hash(const char* str, unsigned int n, unsigned int basis = UINT32_C(2166136261)) {
     return n == 0 ? basis : Hash(str + 1, n - 1, (basis ^ str[0]) * UINT32_C(16777619));
 }
