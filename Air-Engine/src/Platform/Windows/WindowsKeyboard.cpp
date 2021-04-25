30bcaa51edd0f4091a288e6995a6c4ae19f3ebf4
Hyxogen <daansander02@outlook.com>
2021-04-25 17:30:45 +0200

    Remove old code

--- Air-Engine/src/Platform/Windows/WindowsKeyboard.cpp
+++ Air-Engine/src/Platform/Windows/WindowsKeyboard.cpp
@@ -9,23 +9,17 @@
 #include "../../Engine/Util/Logger/Logger.hpp"
 #include "WindowsKeyMappings.hpp"
 
-
 #include <windows.h>
 
 namespace platform {
 	namespace windows {
 
 		WindowsKeyboard::WindowsKeyboard(engine::io::Window* window) : engine::io::Keyboard(window) {
-			/*m_Keys = new unsigned int[platform::windows::KeyCode::NONE];
-
-			memset(m_Keys, 0, sizeof(unsigned int) * (unsigned int)KeyCode::NONE);*/
-
 			engine::core::Application::GetApplication()->GetDispatcher()->Register(Hash("EVENT_KEY_DOWN", 15), this);
 			engine::core::Application::GetApplication()->GetDispatcher()->Register(Hash("EVENT_KEY_RELEASE", 18), this);
 		}
 
 		WindowsKeyboard::~WindowsKeyboard() {
-			//delete m_Keys;
 		}
 
 		bool WindowsKeyboard::GetKeyDown(unsigned int keyCode) const {
