3a8bfb275bf6a9e1e5c2622a3550c890c33e52f7
Hyxogen <daansander02@outlook.com>
2021-04-26 16:25:28 +0200

    Add eventhandling priority values

--- Air-Engine/src/Engine/Events/EventListener.hpp
+++ Air-Engine/src/Engine/Events/EventListener.hpp
@@ -5,10 +5,21 @@ namespace engine {
 
 		class Event;
 
+		enum Priority : unsigned char {
+			PRIORITY_MONITOR		= 0x00,
+			PRIORITY_HIGHEST		= 0x01,
+			PRIORITY_HIGH			= 0x02,
+			PRIORITY_NORMAL			= 0x03,
+			PRIORITY_LOW			= 0x04,
+			PRIORITY_LOWEST			= 0x05
+		};
+
 		class EventListener {
 		public:
 			//If the function returns true, the event dispatcher will stop with passing the current event to other listeners
 			virtual bool OnEvent(Event& event) = 0;
+
+			virtual unsigned char GetPriority() const { return PRIORITY_NORMAL; };
 		};
 
 	}
