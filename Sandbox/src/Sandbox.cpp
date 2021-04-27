02428c864a18e089eec456b59b02837468bf67ce
Hyxogen <daansander02@outlook.com>
2021-04-27 17:07:17 +0200

    Remove tests

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -8,17 +8,7 @@ class Sandbox : public engine::core::Application {
 public:
 
 	Sandbox() {
-		TestEventHandler handler;
-		SuperImportantHandler impHandler;
-		TestEvent event;
-		NearEvent nearEvent;
 
-		this->GetDispatcher()->Register(event.GetID(), &handler);
-		this->GetDispatcher()->Register(event.GetID(), &impHandler);
-		this->GetDispatcher()->Register(nearEvent.GetID(), &handler);
-		this->GetDispatcher()->Register(nearEvent.GetID(), &impHandler);
-		//this->GetDispatcher()->Remove(event.GetID(), &impHandler);
-		this->GetDispatcher()->Dispatch(&nearEvent);
 	}
 
 	~Sandbox() {
