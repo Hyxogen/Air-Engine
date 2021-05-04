4ffc7ce4fc50414e1539eb89be48df0f5f541176
Hyxogen <daansander02@outlook.com>
2021-05-04 16:48:35 +0200

    Add tests

--- Air-Engine/src/Engine/Core/Application.cpp
+++ Air-Engine/src/Engine/Core/Application.cpp
@@ -15,6 +15,8 @@
 
 #include "../Events/EventDispatcher.hpp"
 
+#include "../Math/Vector2.hpp"
+
 #include <glad\wgl.h>
 #include <glad\gl.h>
 
@@ -54,6 +56,17 @@ namespace engine {
 			m_Window = new platform::windows::WindowsWindow(500, 600, L"Application");
 			m_ContextAdapter = new platform::windows::GLContextAdapter((platform::windows::WindowsWindow*)m_Window, 4, 6);
 
+			math::Vector2f vector(std::move(math::Vector2f(0.0f, 5.0f)));
+			math::Vector2f zero = math::Vector2f(5.0f, 0.0f);
+
+			//vector.Multiply(2.0f);
+			math::Vector2f test = vector + zero;
+			math::Vector2 move = vector + zero;
+			float other = 2.0f;
+			vector = vector * other;
+			float movex = move[0];
+			vector.Add(zero);
+
 			if (m_Window->Initialize())
 				return true;
 			if (m_ContextAdapter->Initialize())
@@ -61,6 +74,8 @@ namespace engine {
 
 			m_Window->SetVisibility(AIR_W_SHOW);
 			
+
+
 			AIR_CORE_INFO("Finished initializing");
 			return false;
 		}
