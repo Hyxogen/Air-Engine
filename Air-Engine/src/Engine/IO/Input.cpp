a381d0a81a1a07537aa77005f3d40b04d968566e
Daan Meijer <daansander02@outlook.com>
2021-09-08 12:00:05 +0200

    Add input init error

--- Air-Engine/src/Engine/IO/Input.cpp
+++ Air-Engine/src/Engine/IO/Input.cpp
@@ -12,7 +12,8 @@ namespace engine {
 		Input* Input::s_Instance = nullptr;
 
 		bool Input::Initialize() {
-			AIR_CORE_WARN("This is a simple helper class for handling input. Please be aware that this will most likely be removed or changed once multiple window support gets added")
+			AIR_CORE_WARN("This is a simple helper class for handling input. Please be aware that this will most likely be removed or changed once multiple window support gets added");
+			AIR_CORE_ERROR_IF(core::Application::GetApplication()->GetWindow() == nullptr, "Failed to initialize input handlers, did you create your window?");
 			m_Keyboard = Keyboard::Create(core::Application::GetApplication()->GetWindow());
 			m_Mouse = Mouse::Create(core::Application::GetApplication()->GetWindow());
 			return false;
