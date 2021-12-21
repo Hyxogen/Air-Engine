5c2b3443ab049b1b05aa2ff94dfff8efe76bda70
Daan Meijer <daansander02@outlook.com>
2021-12-21 15:04:42 +0100

    Add assert implementation for linux

--- Air-Engine/src/Engine/Util/Assert.h
+++ Air-Engine/src/Engine/Util/Assert.h
@@ -5,6 +5,9 @@
 
 #define ASSERT(x) if (!x) { } else { DebugBreak(); }
 
+#elif AIR_PLATFORM_LINUX
+#include <signal.h>
+#define ASSERT(x) if (!x) { } else { signal(SIGTRAP); }
 #else
 #define ASSERT(x) x
 #endif
