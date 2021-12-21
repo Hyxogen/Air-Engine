0094e8a125b4265add1462920576f6e5540928ed
Daan Meijer <daansander02@outlook.com>
2021-12-21 15:49:02 +0100

    Fix assert working the wrong way around

--- Air-Engine/src/Engine/Util/Assert.h
+++ Air-Engine/src/Engine/Util/Assert.h
@@ -3,11 +3,11 @@
 #ifdef AIR_PLATFORM_WINDOWS
 #include <Windows.h>
 
-#define ASSERT(x) if (!x) { } else { DebugBreak(); }
+#define ASSERT(x) if (x) { } else { DebugBreak(); }
 
 #elif AIR_PLATFORM_LINUX
 #include <signal.h>
-#define ASSERT(x) if (!x) { } else { signal(SIGTRAP); }
+#define ASSERT(x) if (x) { } else { signal(SIGTRAP); }
 #else
 #define ASSERT(x) x
 #endif
